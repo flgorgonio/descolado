d8821d1db44a8cdfa706ecc41de5e67b91f10706
Flavius Gorgônio <flgorgonio@gmail.com>
2020-09-27 23:20:21 -0300

    Initial commit

---
+++ descolado.c
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                 Developed by @flgorgonio - Sep, 2020                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main(void) {
+	
+	return 0;
+}
